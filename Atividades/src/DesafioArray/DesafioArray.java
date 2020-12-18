9b39fe86864c166e0516ceca81b3f6860d31b491
Israel <israelmessias2016@outlook.com>
2020-12-18 15:58:29 -0300

    Desafio Array comentado

--- Atividades/src/DesafioArray/DesafioArray.java
+++ Atividades/src/DesafioArray/DesafioArray.java
@@ -8,17 +8,19 @@ public class DesafioArray {
         int quantNotas;
         System.out.println("Digite quantidades de notas: ");
         quantNotas = input.nextInt();
-
+        //O tamanho da Array vai o valor da quantNotas
         float[] notas = new float[quantNotas];
-
+        //Esse for ira perguntara o valor da nota de acordo com valor da variavel notas
         for(int i = 0; i < notas.length; i++){
             System.out.println("Digite o valor da nota "+(i+1)+":");
             notas[i] = input.nextInt();
         }
+        //
         float total = 0;
         for(float nota:notas) {
         	total += nota;
         }
+        //media das notas, por total/notas
        float media = total/notas.length;
 
         System.out.println("A media é: "+ media);
