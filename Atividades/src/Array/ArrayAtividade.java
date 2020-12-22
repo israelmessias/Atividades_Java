b6404949b7bd34c7ba49877f74fc0e3a8866eb9c
israelmessias <69429419+israelmessias@users.noreply.github.com>
2020-12-22 18:38:41 -0300

    Novas Atividades Array

---
+++ Atividades/src/Array/ArrayAtividade.java
@@ -0,0 +1,22 @@
+package Array;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ArrayAtividade {
+    public static void main(String[] args) {
+        var input = new Scanner(System.in);
+        int[] array = new int[10];// de 10 posições
+        for (int i = 0; i < array.length; i++) {// Este for perguntara o valor de cada indice
+            System.out.println("Digite um valor " + (i + 1) + ":");
+            array[i] = input.nextInt();
+        }
+        int indice = 0;
+            for (int arrays:array) {
+                System.out.println("Indice: " + indice++ + " -> " + Arrays.toString(new int[]{arrays}));
+
+        }
+
+    }
+}
+
+
--- Atividades/src/Foreach/Foreach.java
+++ Atividades/src/Foreach/Foreach.java
@@ -1,12 +1,15 @@
 package Foreach;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Foreach {
     public static void main(String[] args) {
 
         float[] notas = {9.9f, 9.8f, 7.2f, 9.4f};
         //for a varaiavel nota vai percorrer toda array
         for (float nota: notas){
-            System.out.println(nota);
+            System.out.println(Arrays.toString(new float[]{nota}));
         }
     }
 }
