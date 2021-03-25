8112961f41a9b68a252f04e1eb0c4e40da9ccec6
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-03-25 15:51:51 -0300

    Função Filter

---
+++ Atividades/src/Stream_API/FIlter/Filter.java
@@ -0,0 +1,28 @@
+package Stream_API.FIlter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Filter {
+
+    public static void main(String[] args) {
+        Consumer<Object> println = System.out::println;
+        Aluno a1 = new Aluno("Ana", 7.8);
+        Aluno a2 = new Aluno("Bia", 5.8);
+        Aluno a3 = new Aluno("Daniel", 9.8);
+        Aluno a4 = new Aluno("Gui", 6.8);
+        Aluno a5 = new Aluno("Rebeca", 7.1);
+        Aluno a6 = new Aluno("Pedro", 8.8);
+
+        List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6);
+        Predicate<Aluno> aprovado = a -> a.nota >=7;
+        Function<Aluno, String> parabens = a-> "Parabens "+ a.nome+ "! Você foi aprovado ";
+
+        alunos.stream().filter(aprovado).
+                map(parabens).forEach(println);
+    }
+    
+}
