f0dea45970c265a993eb5f4f1c4fe2f8bbd1cdf5
israelmessias <69429419+israelmessias@users.noreply.github.com>
2021-03-26 06:58:16 -0300

    Classe Aluno

---
+++ Atividades/src/Stream_API/FIlter/Aluno.java
@@ -0,0 +1,13 @@
+package Stream_API.FIlter;
+
+public class Aluno {
+    final String nome;
+    final double nota;
+
+    public Aluno(String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    
+}
