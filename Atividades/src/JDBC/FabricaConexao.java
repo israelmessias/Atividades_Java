ca3e10adae5961ec57a85e665bea9fdfc8c47926
israelmessias <israelmessias2016@outlook.com>
2021-05-20 14:04:23 -0300

    Fabrica de Conexao

--- Atividades/src/JDBC/FabricaConexao.java
+++ Atividades/src/JDBC/FabricaConexao.java
@@ -1,21 +1,35 @@
 package src.JDBC;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.*;
+import java.sql.*;
+import java.util.Properties;
 
 public class FabricaConexao{
     //Classe estatica feita para conexão
-    public static Connection getConexao() throws SQLException {
+    public static Connection getConexao() {
         try{
-            final String url = "jdbc:mysql://localhost/curso_java";
-            final String nome = "israel";
-            final String senha = "Gatopersa6";
+            Properties prop = getProperties();
+            final String url = prop.getProperty("bank.url");
+            final String user = prop.getProperty("bank.user");
+            final String password = prop.getProperty("bank.password");
 
-            return DriverManager.getConnection(url, nome, senha);
-        }catch (SQLException e){
+            return DriverManager.getConnection(url, user, password);
+
+        }catch (SQLException | IOException e){
             throw new RuntimeException(e);
         }
     }
 
+    private static Properties getProperties() throws IOException {
+        //InputStream para leitura de dados || FileInputStream leitura de arquivo
+
+        try{
+            InputStream stream = new FileInputStream
+                    ("/home/israelmessias/Desenvolvimento/Atividades_Java/Atividades/conexao.properties");
+            Properties prop = new Properties();
+            prop.load(stream);
+            return prop;
+        }catch(IOException i){
+            throw new IOException(i);
+        }
+    }
 }
